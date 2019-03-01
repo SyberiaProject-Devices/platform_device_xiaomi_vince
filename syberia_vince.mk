6a9ff64c8e79b5cfe257d4b5a78d8083c6451bf5
mdeejay <mdjrussia@gmail.com>
2019-03-01 15:13:58 +0100

    vince: Switch to MIUI Camera

--- syberia_vince.mk
+++ syberia_vince.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common Syberia stuff.
 $(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
