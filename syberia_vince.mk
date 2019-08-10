120db6f67149ac8a5349599e1046793b16d64b11
mdeejay <mdjrussia@gmail.com>
2019-08-10 12:43:37 +0200

    vince: Don't inherit MiuiCamera
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- syberia_vince.mk
+++ syberia_vince.mk
@@ -23,7 +23,6 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common Syberia stuff.
 $(call inherit-product, vendor/syberia/common.mk)
-$(call inherit-product, vendor/MiuiCamera/config.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
